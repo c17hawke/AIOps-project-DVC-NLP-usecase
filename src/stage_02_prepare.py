ab310eae48dd13722f6bb6604bb856e7ebe98a24
c17hawke <sunny.c17hawke@gmail.com>
2021-11-15 06:23:49 +0530

    st- 02 - base py file ready

---
+++ src/stage_02_prepare.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories
+import random
+
+
+STAGE = "stage 02 prepare data" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    pass
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
